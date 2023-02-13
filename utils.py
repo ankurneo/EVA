c3649e783070071d8056c2208e58ce840abc34a6
EVA <annkur.gupta@gmail.com>
2023-02-13 22:40:13 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,124 +1,95 @@
-'''Some helper functions for PyTorch, including:
-    - get_mean_and_std: calculate the mean and std value of dataset.
-    - msr_init: net parameter initialization.
-    - progress_bar: progress bar mimic xlua.progress.
-'''
-import os
-import sys
-import time
-import math
-
-import torch.nn as nn
-import torch.nn.init as init
-
-
-def get_mean_and_std(dataset):
-    '''Compute the mean and std value of dataset.'''
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    print('==> Computing mean and std..')
-    for inputs, targets in dataloader:
-        for i in range(3):
-            mean[i] += inputs[:,i,:,:].mean()
-            std[i] += inputs[:,i,:,:].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    return mean, std
-
-def init_params(net):
-    '''Init layer parameters.'''
-    for m in net.modules():
-        if isinstance(m, nn.Conv2d):
-            init.kaiming_normal(m.weight, mode='fan_out')
-            if m.bias:
-                init.constant(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            init.constant(m.weight, 1)
-            init.constant(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            init.normal(m.weight, std=1e-3)
-            if m.bias:
-                init.constant(m.bias, 0)
+import torch
+import numpy as np
+import matplotlib.pyplot as plt 
+import warnings
+warnings.filterwarnings('ignore')
+from torchvision import models
+import numpy as np
+import cv2
+import requests
+from torchsummary import summary
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image, \
+    deprocess_image, \
+    preprocess_image
+from PIL import Image
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 
-_, term_width = os.popen('stty size', 'r').read().split()
-term_width = int(term_width)
+def plot_train_vs_test_accuracy(epochs, train_acc, test_acc):
+  train_range = range(1,epochs+1)
+  plt.plot(train_range, train_acc, 'g', label='Training accuracy')
+  plt.plot(train_range, test_acc, 'b', label='validation accuracy')
+  plt.title('Training and Validation accuracy')
+  plt.xlabel('Epochs')
+  plt.ylabel('Accuracy')
+  plt.legend()
+  plt.show()
 
-TOTAL_BAR_LENGTH = 65.
-last_time = time.time()
-begin_time = last_time
-def progress_bar(current, total, msg=None):
-    global last_time, begin_time
-    if current == 0:
-        begin_time = time.time()  # Reset for new bar.
 
-    cur_len = int(TOTAL_BAR_LENGTH*current/total)
-    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+def view_misclassified_images(model, device, dataset, classes):
+  misclassified_images = []
+  
+  for images, labels in dataset:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            for i in range(len(labels)):
+              if(len(misclassified_images)<20 and predicted[i]!=labels[i]):
+                misclassified_images.append([images[i],predicted[i],labels[i]])
+            if(len(misclassified_images)>20):
+              break
+    
+  
+  fig = plt.figure(figsize = (8,8))
+  for i in range(20):
+        sub = fig.add_subplot(5, 5, i+1)
+        #imshow(misclassified_images[i][0].cpu())
+        img = misclassified_images[i][0].cpu()
+        img = img / 2 + 0.5 
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg,(1, 2, 0)),interpolation='none')
+        
+        sub.set_title("P={}, A={}".format(str(classes[misclassified_images[i][1].data.cpu().numpy()]),str(classes[misclassified_images[i][2].data.cpu().numpy()])))
+        
+  plt.tight_layout()
+  return misclassified_images
+  
 
-    sys.stdout.write(' [')
-    for i in range(cur_len):
-        sys.stdout.write('=')
-    sys.stdout.write('>')
-    for i in range(rest_len):
-        sys.stdout.write('.')
-    sys.stdout.write(']')
+#https://jacobgil.github.io/pytorch-gradcam-book/CAM%20Metrics%20And%20Tuning%20Tutorial.html
+#https://www.kaggle.com/code/antwerp/where-is-the-model-looking-for-gradcam-pytorch/notebook
 
-    cur_time = time.time()
-    step_time = cur_time - last_time
-    last_time = cur_time
-    tot_time = cur_time - begin_time
+def model_summary(model, input_size=(3, 32, 32)):
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    print(device)
+    model = model.to(device)
+    summary(model, input_size=input_size)
+	
+def show_grad_cam(model, img, label, classes, mean, std):
+    warnings.filterwarnings('ignore')
+    prediction = model(img).data.cpu().numpy()
+    prediction = np.argmax(prediction)
+    print("Prediction is", classes[prediction])
+    print("Label is", classes[label])
+    img = img.data.permute(1, 2, 0).cpu().numpy()
+    img = cv2.resize(img, (224, 224))
+    img = np.float32(img) / 255
+    input_tensor = preprocess_image(img, mean=mean, std=std)
+    targets = [ClassifierOutputTarget(label)]
+    target_layers = [model.layer4[-1]]
+    with GradCAM(model=model, target_layers=target_layers) as cam:
+        grayscale_cams = cam(input_tensor=input_tensor, targets=targets)
+        cam_image = show_cam_on_image(img, grayscale_cams[0, :], use_rgb=True)
+    cam = np.uint8(255*grayscale_cams[0, :])
+    cam = cv2.merge([cam, cam, cam])
+    images = np.hstack((np.uint8(255*img), cam , cam_image))
+    return Image.fromarray(images)
 
-    L = []
-    L.append('  Step: %s' % format_time(step_time))
-    L.append(' | Tot: %s' % format_time(tot_time))
-    if msg:
-        L.append(' | ' + msg)
-
-    msg = ''.join(L)
-    sys.stdout.write(msg)
-    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
-        sys.stdout.write(' ')
-
-    # Go back to the center of the bar.
-    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
-        sys.stdout.write('\b')
-    sys.stdout.write(' %d/%d ' % (current+1, total))
-
-    if current < total-1:
-        sys.stdout.write('\r')
-    else:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
-
-def format_time(seconds):
-    days = int(seconds / 3600/24)
-    seconds = seconds - days*3600*24
-    hours = int(seconds / 3600)
-    seconds = seconds - hours*3600
-    minutes = int(seconds / 60)
-    seconds = seconds - minutes*60
-    secondsf = int(seconds)
-    seconds = seconds - secondsf
-    millis = int(seconds*1000)
-
-    f = ''
-    i = 1
-    if days > 0:
-        f += str(days) + 'D'
-        i += 1
-    if hours > 0 and i <= 2:
-        f += str(hours) + 'h'
-        i += 1
-    if minutes > 0 and i <= 2:
-        f += str(minutes) + 'm'
-        i += 1
-    if secondsf > 0 and i <= 2:
-        f += str(secondsf) + 's'
-        i += 1
-    if millis > 0 and i <= 2:
-        f += str(millis) + 'ms'
-        i += 1
-    if f == '':
-        f = '0ms'
-    return f
+# Example usage:
+# img = preprocess_image(np.float32(cv2.resize(img, (224, 224))) / 255, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+# print("Prediction is", classes[prediction])
+# print("Label is", classes[label])
+# visualize_grad_cam(model, img, label, classes)
