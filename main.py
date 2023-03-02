3ffd19a6c2019616ea3ecc90a842190bb52a0354
EVA <annkur.gupta@gmail.com>
2023-03-02 19:47:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,195 @@
+import torch                   #PyTorch base libraries
+from torchvision import datasets, transforms
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import torch.nn.functional as F
+import cv2
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import tqdm as tqdm
+from torch.autograd import Variable
+
+from albumentations import Compose, PadIfNeeded, RandomCrop, Normalize, HorizontalFlip, ShiftScaleRotate, CoarseDropout, Cutout, PadIfNeeded
+
+class album_Compose_train():
+    def __init__(self):
+        self.albumentations_transform = Compose([
+            Normalize(mean=[0.4914, 0.4822, 0.4471],std=[0.2469, 0.2433, 0.2615]),
+            ToTensorV2()
+        ])
+    def __call__(self,img):
+        img = np.array(img)
+        img = self.albumentations_transform(image=img)['image']
+        return img
+
+
+class album_Compose_test():
+    def __init__(self):
+        self.albumentations_transform = Compose([
+            Normalize(mean=[0.4914, 0.4822, 0.4471],std=[0.2469, 0.2433, 0.2615]),
+            ToTensorV2()
+        ])
+
+    def __call__(self,img):
+        img = np.array(img)
+        img = self.albumentations_transform(image=img)['image']
+        return img
+
+class dataset_cifar10:
+    """
+    Class to load the data and define the data loader
+    """
+
+    def __init__(self, batch_size=128):
+
+        # Defining CUDA
+        cuda = torch.cuda.is_available()
+        print("CUDA availability ?",cuda)
+
+        # Defining data loaders with setting
+        self.dataloaders_args = dict(shuffle=True, batch_size = batch_size, num_workers = 2, pin_memory = True) if cuda else dict(shuffle=True,batch_size = batch_size)
+        self.sample_dataloaders_args = self.dataloaders_args.copy()
+
+        self.classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    def data(self, train_flag):
+
+        # Transformations data augmentation (only for training)
+        if train_flag :
+            return datasets.CIFAR10('./Data',
+                            train=train_flag,
+                            transform=album_Compose_train(),
+                            download=True)
+
+        # Testing transformation - normalization adder
+        else:
+            return datasets.CIFAR10('./Data',
+                                train=train_flag,
+                                transform=album_Compose_test(),
+                                download=True)
+
+    # Dataloader function
+    def loader(self, train_flag=True):
+        return(torch.utils.data.DataLoader(self.data(train_flag), **self.dataloaders_args))
+
+
+def train(model, device, train_loader, optimizer, epoch, criterion, scheduler, L1):
+    model.train()
+    epoch_loss = 0
+    correct = 0
+
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        # if use_cycle:    
+        #     lr, mom = onecycle.calc()
+        #     update_lr(optimizer, lr)
+        #     update_mom(optimizer, mom)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+
+        if L1:
+          L1_loss = nn.L1Loss(size_average=None, reduce=None, reduction='mean')
+          reg_loss = 0 
+          for param in model.parameters():
+            zero_vector = torch.rand_like(param) * 0
+            reg_loss += L1_loss(param,zero_vector)
+          loss += .001 * reg_loss
+
+        epoch_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+        # update the learning rate
+        scheduler.step()
+        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+
+    print(f'Train set: Average loss: {loss.item():.4f}, Accuracy: {100. * correct/len(train_loader.dataset):.2f}')
+    train_loss = epoch_loss / len(train_loader)
+    train_acc=100.*correct/len(train_loader.dataset)
+    return train_loss, train_acc
+
+def test(model, device, test_loader, criterion):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    test_pred = torch.LongTensor()
+    target_pred = torch.LongTensor()
+    target_data = torch.LongTensor()
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            loss = criterion(output, target)
+            test_loss += loss.item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            pred_cpu = output.cpu().data.max(dim=1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            test_pred = torch.cat((test_pred, pred_cpu), dim=0)
+            target_pred = torch.cat((target_pred, target.cpu()), dim=0)
+            target_data = torch.cat((target_data, data.cpu()), dim=0)
+
+
+    test_loss /= len(test_loader.dataset)
+    test_acc = 100.*correct/len(test_loader.dataset)
+    print(f'\nTest set: Average loss: {test_loss:.3f}, Accuracy: {100. * correct/len(test_loader.dataset):.2f}')
+    return test_loss, test_acc, test_pred, target_pred, target_data
+	
+#import copy
+def main(EPOCHS, model, device, train_loader, test_loader, optimizer, criterion,scheduler, L1):
+  train_loss_values = []
+  test_loss_values = []
+  train_acc_values = []
+  test_acc_values = []
+
+  for epoch in range(EPOCHS):
+      #copied_model = copy.deepcopy(model)
+      print('\nEpoch {} : '.format(epoch))
+      # train the model
+      train_loss, train_acc = train(model, device, train_loader, optimizer, epoch, criterion, scheduler, L1)
+      test_loss, test_acc, test_pred, target_pred, target_data  = test(model, device, test_loader,criterion)
+
+      train_loss_values.append(train_loss)
+      test_loss_values.append(test_loss)
+
+      train_acc_values.append(train_acc)
+      test_acc_values.append(test_acc)
+
+  return train_loss_values, test_loss_values, train_acc_values, test_acc_values, test_pred, target_pred, target_data
+
+def update_lr(optimizer, lr):
+    for g in optimizer.param_groups:
+        g['lr'] = lr
+
+def update_mom(optimizer, mom):
+    for g in optimizer.param_groups:
+        g['momentum'] = mom
+
+def calculate_lr(model, optimizer, trainloader, criterion, clr, device):
+    t = tqdm.tqdm(trainloader, leave=False, total=len(trainloader))
+    running_loss = 0.
+    avg_beta = 0.98
+    model.train()
+    for i, (input, target) in enumerate(t):
+        input, target = input.to(device), target.to(device) 
+        
+        var_ip, var_tg = Variable(input), Variable(target)
+        output = model(var_ip)
+        loss = criterion(output, var_tg)
+        
+        running_loss = avg_beta * running_loss + (1-avg_beta) *loss.item()
+        smoothed_loss = running_loss / (1 - avg_beta**(i+1))
+        t.set_postfix(loss=smoothed_loss)
+        
+        lr = clr.calc_lr(smoothed_loss)
+        if lr == -1 :
+            break
+        update_lr(optimizer, lr)   
+        
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
